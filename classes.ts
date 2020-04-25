c3f9a784cce70fc8f606faa6910871a6e46cb35f
Dmitry White <dmitry.b.white@gmail.com>
2020-04-25 20:21:05 +0300

    Add class inheritance

--- classes.ts
+++ classes.ts
@@ -1,3 +1,4 @@
+// Type for Class: definition
 class Person {
   name: string;
 
@@ -24,5 +25,26 @@ class Person {
 const person = new Person('Dmitry', 'Dmitry-White');
 console.log('Person: ', person);
 
+// Type for Class: public vs private properties
+console.log('Username: ', person.username);
+// console.log(person.type); // Won't work with private properties
+
+// Type for Class: public vs private methods
 person.printAge();
 // person.setType('cool guy'); // Won't work with private methods
+
+// Type for Class: inheritance
+class Dmitry extends Person {
+  constructor(username: string) {
+    super('Dmitry', username);
+    console.log('Age: ', this.age); // Works with protected properties from parent
+    // console.log('Type: ', this.type); // Won't work with private properties
+  }
+}
+
+const dmitry = new Dmitry('DmitryWhite');
+console.log('Dmitry: ', dmitry);
+
+// Type for Class: public vs protected properties
+console.log('Username: ', dmitry.username);
+// console.log(dmitry.age); // Won't work with private properties
