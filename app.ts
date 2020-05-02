ffcc935fbaef2619180bc695b4c2026feb410c59
Dmitry White <dmitry.b.white@gmail.com>
2020-05-02 21:44:51 +0300

    Add namespaces

--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="circleNamespace.ts" />
+/// <reference path="rectangleNamespace.ts" />
+
+import CircleNamespace = MyMath.Circle;
+
 console.log('Hii!');
+console.log(MyMath.calculateRectangle(10, 20));
+console.log(CircleNamespace.calculateCircumference(3));
---
+++ rectangleNamespace.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+
+namespace MyMath {
+  export function calculateRectangle(width: number, height: number): number {
+    return width * height;
+  }
+}
