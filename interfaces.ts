a98ca4bb1a24fecd27bf26c15d0139f95c24b641
Dmitry White <dmitry.b.white@gmail.com>
2020-05-29 23:56:06 +0300

    Add optional or unknown properties

--- interfaces.ts
+++ interfaces.ts
@@ -1,5 +1,7 @@
 interface NamedPerson {
   firstName: string;
+  age?: number;
+  [propName: string]: any; // dynamic - for unknown, but possibly present properties
 }
 
 function greet(person: NamedPerson) {
@@ -7,13 +9,18 @@ function greet(person: NamedPerson) {
 }
 
 function changeName(person: NamedPerson) {
-  person.firstName = 'Viktoria'
+  person.firstName = 'Viktoria';
 }
 
+// Event if "age" is not present in NamedPerson interface
+// (even in age? format or "[key: string]: any" format),
+// variable-assigned objects will not be checked as strictly
+// as literally the same object literal
 const personData = {
   firstName: 'Dmitry',
-  age: 25
-}
+  age: 25,
+  hobbies: ['Music', 'Sports']
+};
 
 greet(personData);
 changeName(personData);
