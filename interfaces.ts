c9c03972bd8214a1584092433c09dac2b425932e
Dmitry White <dmitry.b.white@gmail.com>
2020-05-29 23:57:27 +0300

    Add methods in interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -2,10 +2,11 @@ interface NamedPerson {
   firstName: string;
   age?: number;
   [propName: string]: any; // dynamic - for unknown, but possibly present properties
+  greet(lastName: string): void;
 }
 
 function greet(person: NamedPerson) {
-  console.log(`Interface: Hello, ${person.firstName}`);
+  console.log(`Interfaces: Hello, ${person.firstName}`);
 }
 
 function changeName(person: NamedPerson) {
@@ -16,12 +17,16 @@ function changeName(person: NamedPerson) {
 // (even in age? format or "[key: string]: any" format),
 // variable-assigned objects will not be checked as strictly
 // as literally the same object literal
-const personData = {
+const personData: NamedPerson = {
   firstName: 'Dmitry',
   age: 25,
-  hobbies: ['Music', 'Sports']
+  hobbies: ['Music', 'Sports'],
+  greet(lastName: string): void {
+    console.log(`Interfaces: Hi, I am ${this.firstName} ${lastName}`);
+  },
 };
 
 greet(personData);
 changeName(personData);
 greet(personData);
+personData.greet('White');
