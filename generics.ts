87c40cba8980dec0457ca71ee2aca986e7cfd784
Dmitry White <dmitry.b.white@gmail.com>
2020-05-31 03:26:24 +0300

    Add better generic

--- generics.ts
+++ generics.ts
@@ -6,3 +6,12 @@ function echo(data: any): any {
 console.log(echo('Dmitry'));
 console.log(echo(25));
 console.log(echo({ name: 'Dmitry', age: 25 }));
+
+// Better Generic
+function betterEcho<T>(data: T) {
+  return data;
+}
+
+console.log(betterEcho('Dmitry'));
+console.log(betterEcho<number>(25));
+console.log(betterEcho({ name: 'Dmitry', age: 25 }));
