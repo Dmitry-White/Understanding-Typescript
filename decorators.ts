17908a59316605cb7ddc87994ce52a8a5a619e51
Dmitry White <dmitry.b.white@gmail.com>
2020-06-03 04:51:51 +0300

    Add decorators factory

--- decorators.ts
+++ decorators.ts
@@ -8,3 +8,11 @@ class Human {
     console.log('Hi!');
   }
 }
+
+// Decorator Factory
+function logging(flag: boolean) {
+  return flag ? logged : () => {};
+}
+
+@logging(false) // Car constructor would not be logged
+class Car {}
