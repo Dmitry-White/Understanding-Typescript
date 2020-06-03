76f46baedba3a513c4abbf179b0197af2954c32f
Dmitry White <dmitry.b.white@gmail.com>
2020-06-03 04:57:49 +0300

    Add advanced decorator and append multiple decorators

--- decorators.ts
+++ decorators.ts
@@ -16,3 +16,19 @@ function logging(flag: boolean) {
 
 @logging(false) // Car constructor would not be logged
 class Car {}
+
+// Advanced decorator
+function printable(constructorFn: Function) {
+  constructorFn.prototype.print = function() {
+    console.log('Decorators: ', this);
+  }
+}
+
+@logging(true)
+@printable
+class SmallPlant {
+  name = 'Green Plant'
+}
+
+const smallPlant = new SmallPlant();
+(<any>smallPlant).print();
